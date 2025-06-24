addc50a4ff22092751779fd7a8bae6a9a4cb906b
Anupam <anupamkandel5@gmail.com>
2025-06-24 19:37:02 +0545

    a

--- frontend/src/components/AuthImagePattern.jsx
+++ frontend/src/components/AuthImagePattern.jsx
@@ -1,6 +1,6 @@
 const AuthImagePattern = ({ title, subtitle }) => {
   return (
-    <div className="hidden lg:flex items-center justify-center bg-base-200 p-12">
+    <div className="hidden lg:flex lg:flex-col items-center justify-center bg-base-200 p-12">
       <h2 className="text-2xl font-bold mb-4">{title}</h2>
       <p className="text-base-content/60">{subtitle}</p>
     </div>
